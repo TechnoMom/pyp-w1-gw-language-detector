32a07e75fc992e675b9a18426409965de13e548c
Cyn Armistead <cyn@cynthiaarmistead.com>
2017-03-26 18:42:54 +0000

    initial commit

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -28,4 +28,42 @@ LANGUAGES = [
     },
     #  include the English language here
     # HINT: https://en.wikipedia.org/wiki/Most_common_words_in_English
-]
+     {
+        'name': 'English',
+        'common_words': [
+            'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 
+            'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 
+            'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 
+            'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would',
+            'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who',
+            'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time',
+            'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 
+            'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than',
+            'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think',
+            'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 
+            'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any',
+            'these', 'give', 'day', 'most', 'us'
+            ]
+        },
+                {
+            'name': 'French',
+            'common_words': [
+                'le', 'de', 'un', 'à', 'être', 'et', 'en', 'avoir', 'que',
+                'pour', 'dans', 'ce', 'il', 'qui', 'ne', 'sur', 'se', 'pas',
+                'plus', 'pouvoir', 'par', 'je', 'avec', 'tout', 'faire',
+                'son', 'mettre', 'autre', 'on', 'mais', 'nous', 'comme',
+                'ou', 'si', 'leur', 'y', 'dire', 'elle', 'devoir', 'avant',
+                'deux', 'même', 'prendre', 'aussi', 'celui', 'donner',
+                'bien', 'où', 'fois', 'vous', 'encore', 'nouveau', 'aller',
+                'cela', 'entre', 'premier', 'vouloir', 'déjà', ', grand',
+                'mon', 'me', 'moins', 'aucun', 'lui', 'temps', 'très',
+                'savoir', 'falloir', 'voir', 'quelque', 'sans', 'raison',
+                'notre', 'dont', 'non', 'an', 'monde', 'jour', 'monsieur',
+                'demander', 'alors', 'après', 'trouver', 'personne',
+                'rendre', 'part', 'dernier', 'venir', 'pendant', 'passer',
+                'peu', 'lequel', 'suite', 'bon', 'comprendre', 'depuis',
+                'point', 'ainsi', 'heure', 'rester'
+                ]
+          }
+        
+        ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -2,7 +2,28 @@
 
 """This is the entry point of the program."""
 
+from .languages import LANGUAGES
+
+def count_words(list_of_words, language):
+    count = 0
+    for word in list_of_words:
+        if word in language:
+            count += 1
+    return count
+    
+
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    temp_text = text.split()
+    word_list = []
+    for word in temp_text:
+        word = word.strip("-,()-.")
+        word_list.append(word.lower())
+        
+    #count words per language
+    count_of_words = {}
+    for language in languages:
+        count_of_words[language['name']] = count_words(word_list, language['common_words'])
+        
+    return max(count_of_words.keys(), key=(lambda key: count_of_words[key]))
